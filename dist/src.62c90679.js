eb3b9d6cc680907467800358d80969e115fc15c5
dorachan <dora.l.chan@gmail.com>
2018-07-15 00:35:17 -0700

    run dev

--- dist/src.62c90679.js
+++ dist/src.62c90679.js
@@ -19867,7 +19867,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '49410' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '49467' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
