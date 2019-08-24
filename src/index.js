14ac22c9fa892755f140cbf898da271eeef9d480
Dora Chan <dora.l.chan@gmail.com>
2019-08-23 17:39:06 -0700

    wipe out

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import Redbox from 'redbox-react';
 
-const Index = () => {
-  return <div>Hello React!</div>;
+import Presentation from './presentation';
+
+const CustomErrorReporter = ({ error }) => <Redbox error={error} />;
+
+CustomErrorReporter.propTypes = {
+  error: PropTypes.instanceOf(Error).isRequired,
 };
 
-ReactDOM.render(<Index />, document.getElementById("index"));
+ReactDOM.render(
+  <AppContainer errorReporter={CustomErrorReporter}>
+    <Presentation />
+  </AppContainer>,
+  document.getElementById('root')
+);
+
+if (module.hot) {
+  module.hot.accept('./presentation', () => {
+    const NextPresentation = require('./presentation').default;
+    ReactDOM.render(
+      <AppContainer errorReporter={CustomErrorReporter}>
+        <NextPresentation />
+      </AppContainer>,
+      document.getElementById('root')
+    );
+  });
+}
